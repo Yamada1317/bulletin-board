2235845db19b3b6b8cfbcafb2459a2b0e5e9ab78
Yamada1317 <66938620+Yamada1317@users.noreply.github.com>
2021-10-20 15:57:43 +0900

    main base create

---
+++ client/js/main.js
@@ -0,0 +1,11 @@
+$('#post').on('click', () =>{
+    alert("投稿が押されました");
+});
+
+$('.card .edit').on('click', () =>{
+    alert("編集が押されました");
+});
+
+$('.card .delete').on('click', () =>{
+    alert("削除が押されました");
+});
