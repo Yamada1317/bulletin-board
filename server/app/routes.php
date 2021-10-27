1c72ead31fcdda7e4cb309301fe17da90150259d
Yamada1317 <66938620+Yamada1317@users.noreply.github.com>
2021-10-27 15:57:59 +0900

    10/27 2

--- server/app/routes.php
+++ server/app/routes.php
@@ -14,6 +14,29 @@ return function (App $app) {
         return $response;
     });
 
+    //投稿一覧の取得API
+    $app->get('/api/page', function (Request $request, Response $response) {
+        $link = mysqli_connect('localhost','root','','bulletin-board');
+        $result = mysqli_query($link,'SELECT * FROM messages');
+        $messages = mysqli_fetch_all($result;);
+        mysqli_close($link);
+
+        //文字列(json形式)にして返す
+        $response->getBody()->write(json_encode($messages,JSON_UNESCAPED_UNICODE));
+        return $response;
+
+    });
+
+    //新規投稿API
+    $app->post('/api/page', function (Request $request, Response $response) {
+        
+        $link = mysqli_connect('localhost','root','','bulletin-board');
+        $stmt = mysqli_prepare($link,"INSERT INTO massages (title, message) VALUES (?,?)");
+        mysqli_stmt_bind_param($stmt, "ss", $name, $kana);
+        mysqli_close($link);
+    });
+
+
     $app->get('/', function (Request $request, Response $response) {
         $response->getBody()->write('Hello world!');
         return $response;
